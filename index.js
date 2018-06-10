1e6d2cfadb82f71024150ad6c329081e67c64f59
Leandro Nunes <leandrof@ciandt.com>
2018-06-10 20:35:24 -0300

    chore: update navigation version

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { YellowBox } from 'react-native'
 YellowBox.ignoreWarnings([
   'Warning: isMounted(...) is deprecated',
   'Module RCTImageLoader',
+  'Class RCTCxxModule', // https://github.com/facebook/react-native/issues/18201#issuecomment-390427365
 ])
 
 AppRegistry.registerComponent('CiandtPeople', () => App);
